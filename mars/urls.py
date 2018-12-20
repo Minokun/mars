b96ec745987e4d562cfbe929aec8cbe46922e745
wuxiaokun <xiaokun.wu@monstar.lab.com.cn>
2018-12-20 22:12:23 +0800

    修改bug

--- mars/urls.py
+++ mars/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('', admin.site.urls),
     url('users/', include('users.urls')),
 ]
